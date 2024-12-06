dca079a3e96367b3ef23dcbea4ec8f5afaee04e2
Rem19Yulia <kyv190591@gmail.com>
2024-12-06 07:51:29 +0300

    Update task.py

--- task.py
+++ task.py
@@ -1,1 +1,23 @@
+from fastapi import APIRouter
 
+router = APIRouter(prefix="/task", tags=["task"])
+
+@router.get("/")
+def all_tasks():
+    pass  # Здесь будет логика для получения всех задач
+
+@router.get("/{task_id}")
+def task_by_id(task_id: int):
+    pass  # Здесь будет логика для получения задачи по ID
+
+@router.post("/create")
+def create_task():
+    pass  # Здесь будет логика для создания задачи
+
+@router.put("/update")
+def update_task():
+    pass  # Здесь будет логика для обновления задачи
+
+@router.delete("/delete")
+def delete_task():
+    pass  # Здесь будет логика для удаления задачи
